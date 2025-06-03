9d14e330686ac3592e2084f330018e50c88f6a7e
ruru <ruru.dev07@gmail.com>
2025-06-03 15:21:19 +0530

    aaaaa

--- packages/eslint-config/next.js
+++ packages/eslint-config/next.js
@@ -5,7 +5,28 @@ import pluginReactHooks from "eslint-plugin-react-hooks";
 import pluginReact from "eslint-plugin-react";
 import globals from "globals";
 import pluginNext from "@next/eslint-plugin-next";
-import { config as baseConfig } from "./base.js";
+
+const baseConfig = [
+  js.configs.recommended,
+  eslintConfigPrettier,
+  ...tseslint.configs.recommended,
+  {
+    plugins: {
+      turbo: turboPlugin,
+    },
+    rules: {
+      "turbo/no-undeclared-env-vars": "warn",
+    },
+  },
+  {
+    plugins: {
+      onlyWarn,
+    },
+  },
+  {
+    ignores: ["dist/**"],
+  },
+];
 
 /**
  * A custom ESLint configuration for libraries that use Next.js.
